96d27090ebbb1aad003f1804b76cbffe43a66fc9
Bob Nguyen <mrduynguyen235@gmail.com>
2020-11-21 12:48:08 -0600

    add all function for password generator

--- script.js
+++ script.js
@@ -1,21 +1,90 @@
 //All Characters String
-var SpecChar = " !\"#$%&\'()*+,-.\/:;<=>?@[\\]^_`{|}~" ;
-var LowChar = "abcdefghijklmnopqrstuvwxyz";
-var UpChar = LowChar.toUpperCase();
-var NumChar = "0123456789";
-var MixedChar = SpecChar + LowChar + UpChar + NumChar;
+var SpecChar = {
+  Content: " !\"#$%&\'()*+,-.\/:;<=>?@[\\]^_`{|}~" ,
+  Name: "SPECIAL CHARACTERS",
+}
+var LowChar = {
+  Content: "abcdefghijklmnopqrstuvwxyz",
+  Name: "LOWER CHARACTERS",
+}
+var UpChar = {
+  Content: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  Name:"UPPER CHARACTERS",
+}
+var NumChar = {
+  Content: "0123456789",
+  Name: "NUMBERS",
+}
 
-//String to Array
-var SpecCharArr = SpecChar.split("");
-var LowCharArr = LowChar.split("");
-var UpCharArr = UpChar.split("");
-var NumCharArr = NumChar.split("");
-var MixedCharArr = MixedChar.split("");
+//Pick One Random Character in a string then add it into another string
+function AddRandomOneChar(CharString, StringResult)
+{
+  var CharStringArr = CharString.split("");
+  var RandomPos = Math.floor(Math.random() * (CharStringArr.length-1) );
+  StringResult.Content += CharStringArr[RandomPos];
+  console.log(StringResult.Content)
+}
 
+
+//All Password Critia
+function AskCritia(Critia, MixedChar, password)
+{
+  while (true) {
+    var IsCritia = prompt("Do you want your password contain "+ Critia.Name +" ? (y/n)").toLowerCase();
+    if (IsCritia == 'y') {
+      MixedChar.Content += Critia.Content;
+      AddRandomOneChar(Critia.Content, password)
+      break;
+    }
+    else if (IsCritia == 'n') {
+      break;
+    }
+    else{
+      continue;
+    }
+  }
+
+
+}
 
 //Main Code Generator
 function generatePassword()
 {
+  var password ={
+    Content: "",
+    Length: 0,
+  }
+  var MixedChar = {
+    Content: ""
+  }
+  console.log(MixedChar);
+  AskCritia(UpChar, MixedChar, password); console.log(password);
+  AskCritia(LowChar, MixedChar, password); console.log(password);
+  AskCritia(NumChar, MixedChar, password); console.log(password);
+  AskCritia(SpecChar, MixedChar, password); console.log(password);
+  do{
+    password.Length = prompt ("Enter the LENGTH for your password: (8 to 128)")  
+  } while ((password.Length<8) || (password.Length>128));
+  
+  while (password.Content.length < password.Length){
+    AddRandomOneChar(MixedChar.Content, password)
+  }
+  //Mix for final password
+  var realpassword ="";
+  while (password.Content.length > 0)
+  {
+    //Get a character in password.Content and add it to the real password
+    var RandomPos = Math.floor(Math.random() * (password.Content.length-1))
+    realpassword += password.Content.substr(RandomPos,1)
+    //Get a new password.Content after take out 1 random character
+    var substr1 = password.Content.substring(0,RandomPos)
+    var substr2 = password.Content.substring(RandomPos+1, password.Content.length)
+    password.Content=substr1.concat(substr2);
+   
+  }
+
+
+  return realpassword;
 
 }
 
